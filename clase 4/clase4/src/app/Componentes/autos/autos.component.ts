25425ebab12df2bdf6f7c3e517d379edb5f20a2a
alexsam86 <sa.alayoubi@duocuc.cl>
2022-09-02 18:02:36 -0400

    tercer commit clase 4

--- clase 4/clase4/src/app/Componentes/autos/autos.component.ts
+++ clase 4/clase4/src/app/Componentes/autos/autos.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import {Auto} from './../../modelos/auto';
 @Component({
   selector: 'app-autos',
   templateUrl: './autos.component.html',
   styleUrls: ['./autos.component.scss']
 })
-export class AutosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class AutosComponent {
+  @Input() public auto!: Auto;
 
 }
